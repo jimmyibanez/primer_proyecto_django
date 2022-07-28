6015dd0a62c59486bf1ff8740bffdf2050a54e96
Jimmy Ibañez <jimmyibanez@glm.edu.co>
2022-07-27 21:46:33 -0500

    inicio del primer modelo

---
+++ family/data_family/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DataFamilyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'data_family'
--- family/family/views.py
+++ family/family/views.py
@@ -1,5 +1,11 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def prueba(request):
     return render(request,'prueba.html', context={})
+
+def relatives(request):
+    context= {
+        
+    }
